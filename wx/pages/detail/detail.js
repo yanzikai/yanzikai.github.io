cf55752f8b212161148311e5c64b9dad4124ecbc
yanzikai <835942323@qq.com>
2018-03-16 15:11:45 +0800

    wx over

--- wx/pages/detail/detail.js
+++ wx/pages/detail/detail.js
@@ -1,4 +1,4 @@
-// pages/index/index.js
+// pages/detail/detail.js
 Page({
 
   /**
@@ -6,13 +6,13 @@ Page({
    */
   data: {
     imgarr: [
-      'http://jys.m0571.com/img/gg1.png',
-      'http://jys.m0571.com/img/gg1.png',
-      'http://jys.m0571.com/img/gg1.png',
+      'http://jys.m0571.com/img/wine.png',
+      'http://jys.m0571.com/img/indexbanner1.png',
+      'http://jys.m0571.com/img/wine.png',
       'http://jys.m0571.com/img/gg1.png'
     ],
-    textarr:[{
-      text:'这是新闻一'
+    textarr: [{
+      text: '这是新闻一'
     },
     {
       text: '这是新闻二'
@@ -25,17 +25,7 @@ Page({
     duration: 1000,
     indicatorDots: true
   },
-  goadvert:function(){
-    wx:wx.navigateTo({
-      url: '../advert/advert'
-    })
-  },
-  godetail: function () {
-    wx: wx.navigateTo({
-      url: '../detail/detail'
-    })
-  },
-  previewImage: function (e) {
+  previewImage:function(e){
     var current = e.target.dataset.src;
     wx.previewImage({
       current: current,
@@ -46,55 +36,55 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
